c54f019fadfa493547b706149a0efedb0f951057
Mariana Martínez <marianamtzdelgado@macbook-air-de-mariana.local>
2016-03-26 22:00:57 -0600

    Initial Commit

---
+++ Hamburguesas en el Mundo!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hamburguesas en el Mundo!
+//
+//  Created by Mariana Martínez Delgado on 26/03/16.
+//  Copyright © 2016 MMD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
